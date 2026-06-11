c0acabe6aa6165260656b0844ea9582c560e538f
hAG-stack <hager.elmahrouki1@gmail.com>
2026-06-12 01:50:54 +0300

    Add files via upload

---
+++ task2_q1.py
@@ -0,0 +1,12 @@
+temp= int(input("Enter the current temprature"))
+if temp>= 30 : 
+    print("It's a hot day. Stay hydrated!")
+
+elif temp>=20 and temp<=29 :
+    print("It's a warm day. Enjoy the weather!")
+
+elif temp>=10 and temp<=19 :
+    print("It's a cool day. Wear a jacket!")
+
+else:
+    print("It's a cold day. Stay warm!")
